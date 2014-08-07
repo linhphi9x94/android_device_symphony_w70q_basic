027060cf389e3c1981a8705a161573c9bed40603
Linh Phi <linhphi9x94@gmail.com>
2014-08-07 22:04:48 +0700

    device tree for w70q build cm recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ TARGET_CPU_VARIANT := cortex-a7
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := w70q
-
+DEVICE_RESOLUTION := 480x640
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
